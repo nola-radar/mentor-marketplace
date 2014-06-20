0c0452b759a5d06cbd6d203bcf9fce589c9f72f8
Marcus Bischof <marcus.bischof@ge.com>
2014-06-20 11:11:27 -0500

    added template command to create_bd.sql

--- src/main/scripts/create_db.sql
+++ src/main/scripts/create_db.sql
@@ -1,10 +1,11 @@
--- I would recommend using pgAdmin to create the database,
+﻿-- I would recommend using pgAdmin to create the database,
 -- then copy the sql from the 'SQL Pane' to here
 -- so we have scripts that we can run on the servers.
 -- Probably won't need anymore databases though.
 
 CREATE DATABASE mentormarketplace
   WITH OWNER = ideavillage
+       TEMPLATE = template0
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        LC_COLLATE = 'C'
