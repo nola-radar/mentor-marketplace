f072d8bab25e1e25e1d7ee7e02a79b57daf18405
Nicholas Dupuis <Nicholas.Dupuis@ge.com>
2014-07-23 12:46:01 -0500

    Added Admin dashboard view, AdminController, test package for admin controller

---
+++ src/main/java/org/ideavillage/mentormarketplace/web/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package org.ideavillage.mentormarketplace.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author 212412062
+ */
+
+// Tells Spring that this class is an MVC Controller
+@Controller
+// Tells Spring to route all "/mentormarketplace/founders/*" requests to this controller.
+@RequestMapping(value = "/admin")
+public class AdminController {
+    
+    
+    
+}
---
+++ src/test/java/org/ideavillage/mentormarketplace/web/controllers/AdminControllerTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ideavillage.mentormarketplace.web.controllers;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author 212412062
+ */
+public class AdminControllerTest extends TestCase {
+    
+    public AdminControllerTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
