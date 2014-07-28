6313713caa7922f925f4f267b16f451f23ab2754
Mark Henderson <212391758@CMF-42XDXY1.logon.ds.ge.com>
2014-07-28 11:04:55 -0500

    Created Branch from US3302 and added my Tests

--- src/main/java/org/ideavillage/mentormarketplace/core/domain/LinkedInProfile.java
+++
@@ -1,9 +0,0 @@
-package org.ideavillage.mentormarketplace.core.domain;
-
-/**
- *
- * @author 212395189
- */
-public class LinkedInProfile {
-    
-}
---
+++ src/test/java/views/mentor/SignupMentorTest.java
@@ -0,0 +1,78 @@
+
+
+import java.io.IOException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.AfterClass;
+
+/**
+ *
+ * @author 212391758
+ */
+
+public class SignupMentorTest {
+
+    WebDriver driver;
+
+    /**
+     * Starts a new instance of Chromedriver and directs it to Mentor Market
+     * Place
+     */
+    @BeforeClass
+    public void setUp() {
+        System.out.println("Setting up webdriver");
+        
+        //Path to the Chromedriver executable
+        System.setProperty("webdriver.chrome.driver", "C:/Users/212391758/Desktop/chromedriver.exe");
+        String baseUrl = "http://localhost:8080/mentormarketplace/";
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
+    
+    /**
+     * Closes the opened browser after testing is completed
+     * @throws IOException 
+     */
+    @AfterClass
+    public void closeBrowser() throws IOException {
+        driver.quit();
+
+    }
+
+    /**
+     * 
+     * @throws InterruptedException 
+     */
+    @Test(groups = {"Founder Signup"})
+    public void testSignupFounder() throws InterruptedException {
+          String title;
+       //Check to see if we are on the index page        
+        title = "Mentor Marketplace";
+        
+        assertTrue(title.equals(driver.getTitle()));
+        
+        driver.findElement(By.id("indexButton")).click();
+                
+        //Check to see if we are on the LinkedIn authorization page
+        title = "Authorize | LinkedIn";
+        assertTrue(title.equals(driver.getTitle()));
+
+        //Put your LinkedIn information here for login
+        String userName = "tyler.bailey1@ge.com";
+        String password = "tyler123";
+        
+        driver.findElement(By.id("session_key-oauth2SAuthorizeForm")).sendKeys(userName);
+        driver.findElement(By.id("session_password-oauth2SAuthorizeForm")).sendKeys(password);        
+        driver.findElement(By.name("authorize")).click();
+                
+    }
+
+}
+
+    
+
